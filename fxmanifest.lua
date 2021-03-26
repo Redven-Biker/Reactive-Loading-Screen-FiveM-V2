ba9b2fb1c5fb7630338e9126859a7ee61b09389d
Redven-Biker <marween35620@gmail.com>
2021-03-26 06:29:11 +0100

    1.1
    - Change to fxmanifest.lua
    - fix music

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/*.js',
+    'img/background.jpg',
+    'img/logo.png',
+    'music/music.mp3'
+}
